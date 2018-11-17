c14954543346c738a7c9f3b5ef129c00bf1c1aef
ezreal <tornike.zhizhiashvili@geolab.edu.ge>
2018-11-17 20:12:11 +0400

    no message

--- js/myjs.js
+++ js/myjs.js
@@ -288,26 +288,26 @@ hideNav()
 
 
 
-mousemove
+// mousemove
 
-function mousemove(){
+// function mousemove(){
 
 
-window.addEventListener("mousemove",function(event){
-var x = (event.clientX / window.innerWidth *100)/25;
-var y = (event.clientY / window.innerWidth *100)/15;
+// window.addEventListener("mousemove",function(event){
+// var x = (event.clientX / window.innerWidth *100)/25;
+// var y = (event.clientY / window.innerWidth *100)/15;
 
-document.getElementById("penbox").style.top = 2+ y + "%";
-document.getElementById("penbox").style.right = 5+x + "%";
+// document.getElementById("penbox").style.top = 2+ y + "%";
+// document.getElementById("penbox").style.right = 5+x + "%";
 
-document.getElementById("pigeon").style.top = 7+ x + "%";
-document.getElementById("pigeon").style.left = 15+y + "%";
+// document.getElementById("pigeon").style.top = 7+ x + "%";
+// document.getElementById("pigeon").style.left = 15+y + "%";
 
-})
+// })
 
-}
+// }
 
-mousemove()
+// mousemove()
 
   
 })
@@ -316,21 +316,21 @@ mousemove()
 
 
 
-function mousemove3(){
+// function mousemove3(){
 
 
-window.addEventListener("mousemove",function(event){
-var x = (event.clientX / window.innerWidth *100)/25;
-var y = (event.clientY / window.innerWidth *100)/10;
+// window.addEventListener("mousemove",function(event){
+// var x = (event.clientX / window.innerWidth *100)/25;
+// var y = (event.clientY / window.innerWidth *100)/10;
 
-document.getElementById("bio_move").style.top = 5+ x + "%";
-document.getElementById("bio_move").style.left = 2+y + "%";
+// document.getElementById("bio_move").style.top = 5+ x + "%";
+// document.getElementById("bio_move").style.left = 2+y + "%";
 
-})
+// })
 
-}
+// }
 
-mousemove3()
+// mousemove3()
 
 
 
